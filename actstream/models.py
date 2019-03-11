394b12ee437d1f073b31ca83871f0bf851388766
pandabearcoder <vlouie00@gmail.com>
2019-03-11 11:10:25 +0100

    Add default value for custom fields for backwards compatibility

--- actstream/models.py
+++ actstream/models.py
@@ -127,10 +127,10 @@ class Action(models.Model):
 
     public = models.BooleanField(default=True, db_index=True)
 
-    title = models.CharField(max_length=256, blank=True)
-    context_message = models.CharField(max_length=256, blank=True)
-    primary_url = models.CharField(max_length=256, blank=True)
-    secondary_url = models.CharField(max_length=256, blank=True)
+    title = models.CharField(max_length=256, blank=True, default='')
+    context_message = models.CharField(max_length=256, blank=True, default='')
+    primary_url = models.CharField(max_length=256, blank=True, default='')
+    secondary_url = models.CharField(max_length=256, blank=True, default='')
 
     objects = actstream_settings.get_action_manager()
 
